82f7facce87a9516f5516f14779aaf3538633680
Mark <mark@iMac2.local>
2018-10-22 08:59:07 -0400

    i got it to some what print out the prime numbers in a new list

--- assignment_six.py
+++ assignment_six.py
@@ -9,6 +9,12 @@ def main():
     for x in range(2, max_num + 1):
         blank_list.append(x)
     print(blank_list)
-
-
+    prime_number = []
+    for max_num in range(2, max_num):
+        for y in range(2, max_num):
+            if (max_num % y) == 0:
+                break
+            else:
+                prime_number.append(max_num)
+    print(prime_number)
 main()
