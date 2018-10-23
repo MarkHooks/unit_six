f9970925aee3c53a1386ac01e29b994d0ea5c4fc
Mark <mark@iMac2.local>
2018-10-23 09:12:42 -0400

    modifying a list

---
+++ d3_unit_six.py
@@ -0,0 +1,6 @@
+students = ["harry", "ron", "hermione", "ginny", "fred", "neville", "draco"]
+students.remove("fred")
+del students[4]
+students[1] = "bill"
+students.append("parvati")
+print(students)
