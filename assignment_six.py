48cf808aa87e7885a456d59488fee5f9e012c051
Mark <mark@iMac2.local>
2018-10-23 10:15:21 -0400

    attempetd to get it to print prime numbers

--- assignment_six.py
+++ assignment_six.py
@@ -10,11 +10,21 @@ def main():
         blank_list.append(x)
     print(blank_list)
     prime_number = []
-    for max_num in range(2, max_num):
-        for y in range(2, max_num):
-            if (max_num % y) == 0:
-                break
-            else:
-                prime_number.append(max_num)
+    while len(blank_list) > 0:
+        number = blank_list[0]
+        prime_number.append(number)
+        for z in blank_list:
+            if z % blank_list[0] == 0:
+                blank_list.remove(z)
+        # for max_num in range(2, max_num):
+        #     for y in range(2, max_num):
+        #         if max_num % y == 0:
+        #             del blank_list[y]
+        #             break
+        #         else:
+        #             prime_number.append(max_num)
+
     print(prime_number)
+
+
 main()
