60d0cbb386e9b21921fabf815aade4b47d46d3db
Mark <mark@iMac2.local>
2018-10-24 10:41:15 -0400

    finnished project

--- assignment_six.py
+++ assignment_six.py
@@ -1,30 +1,28 @@
+# this program is to create a list of prime numbers from the maximum number that the user gives
 def user_number():
-    max_num = input("what should the maximum number be?")
+    """
+    this program is to get the user to give the maximum number
+    :return: this returns the function
+    """
+    max_num = int(input("type in the maximum number."))
     return max_num
 
 
 def main():
-    max_num = int(input("what should the maximum number be?"))
+    max_num = int(input("type in the maximum number."))
     blank_list = []
     for x in range(2, max_num + 1):
         blank_list.append(x)
     print(blank_list)
     prime_number = []
+    # this is to loop through the list
     while len(blank_list) > 0:
         number = blank_list[0]
         prime_number.append(number)
         for z in blank_list:
-            if z % blank_list[0] == 0:
+            if z % number == 0:
                 blank_list.remove(z)
-        # for max_num in range(2, max_num):
-        #     for y in range(2, max_num):
-        #         if max_num % y == 0:
-        #             del blank_list[y]
-        #             break
-        #         else:
-        #             prime_number.append(max_num)
-
-    print(prime_number)
+    print("your list of prime numbers is", prime_number)
 
 
 main()
